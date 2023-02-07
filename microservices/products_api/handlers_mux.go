871ae92e7465e980ea51581517ee5ff8db7d80e4
johnnrails <joaocarfe9@gmail.com>
2023-02-07 17:25:50 -0300

    replace router handlers from standard library for gorilla/mux handlers

---
+++ microservices/products_api/handlers_mux.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+	"regexp"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+type ProductHandlerMux struct {
+	l *log.Logger
+}
+
+func NewProductHandlerMux(l *log.Logger) *ProductHandlerMux {
+	return &ProductHandlerMux{l}
+}
+
+func (h *ProductHandlerMux) GetIDFromPath(path string) (int, error) {
+	reg := regexp.MustCompile("/([0-9]+)")
+	g := reg.FindAllStringSubmatch(path, -1)
+
+	moreThanOneId := len(g) != 1
+	if moreThanOneId {
+		return -1, errors.New("More than one ID Found.")
+	}
+
+	moreThanOneCaptureGroup := len(g[0]) != 2
+	if moreThanOneCaptureGroup {
+		return -1, errors.New("More than one capture group found.")
+	}
+
+	idString := g[0][1]
+	id, err := strconv.Atoi(idString)
+
+	if err != nil {
+		return -1, errors.New("Unable to parse ID to Int.")
+	}
+
+	return id, nil
+}
+
+func (h *ProductHandlerMux) GetProducts(w http.ResponseWriter, r *http.Request) {
+	h.l.Println("Handle GET Products")
+	products := GetProducts()
+	if err := products.ToJSON(w); err != nil {
+		http.Error(w, "Unable to marshal json", http.StatusInternalServerError)
+	}
+}
+
+func (h *ProductHandlerMux) AddProduct(w http.ResponseWriter, r *http.Request) {
+	h.l.Println("Handle POST Product")
+	product := r.Context().Value(KeyProduct{}).(Product)
+	AddProduct(&product)
+}
+
+func (h *ProductHandlerMux) UpdateProduct(w http.ResponseWriter, r *http.Request) {
+	h.l.Println("Handle PUT Product")
+
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, "Unable to convert id", http.StatusBadRequest)
+		return
+	}
+
+	product := r.Context().Value(KeyProduct{}).(Product)
+	err = UpdateProduct(id, &product)
+	if err != nil {
+		http.Error(w, "Product Not Found", http.StatusNotFound)
+		return
+	}
+}
+
+func (h ProductHandlerMux) MiddlewareValidateProduct(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		product := Product{}
+		if err := product.FromJSON(r.Body); err != nil {
+			h.l.Println("[ERROR] deserializing product", err)
+			http.Error(w, "Error reading product", http.StatusBadRequest)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), KeyProduct{}, product)
+		r = r.WithContext(ctx)
+
+		next.ServeHTTP(w, r)
+	})
+}
--- microservices/products_api/main.go
+++ microservices/products_api/main.go
@@ -7,6 +7,8 @@ import (
 	"os"
 	"os/signal"
 	"time"
+
+	"github.com/gorilla/mux"
 )
 
 var addr = ":9090"
@@ -15,10 +17,20 @@ func main() {
 	l := log.New(os.Stdout, "products-api", log.LstdFlags)
 
 	// create the handlers
-	ph := NewProductHandler(l)
+	ph := NewProductHandlerMux(l)
 
-	sm := http.NewServeMux()
-	sm.Handle("/", ph)
+	sm := mux.NewRouter()
+
+	getRouter := sm.Methods(http.MethodGet).Subrouter()
+	getRouter.HandleFunc("/", ph.GetProducts)
+
+	putRouter := sm.Methods(http.MethodPut).Subrouter()
+	putRouter.HandleFunc("/{id:[0-9]+}", ph.UpdateProduct)
+	putRouter.Use(ph.MiddlewareValidateProduct)
+
+	postRouter := sm.Methods(http.MethodPost).Subrouter()
+	postRouter.HandleFunc("/", ph.AddProduct)
+	postRouter.Use(ph.MiddlewareValidateProduct)
 
 	s := http.Server{
 		Addr:         addr,              // configure the bind address
