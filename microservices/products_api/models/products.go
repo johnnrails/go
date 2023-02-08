7579afc2b7f294896dd5674c9cb7aec0e168254d
johnnrails <joaocarfe9@gmail.com>
2023-02-08 14:48:38 -0300

    removed product_funk.go file and moved all functions to products.go

--- microservices/products_api/models/products.go
+++ microservices/products_api/models/products.go
@@ -1,8 +1,11 @@
 package models
 
 import (
+	"errors"
 	"fmt"
 	"time"
+
+	"github.com/thoas/go-funk"
 )
 
 type Product struct {
@@ -38,6 +41,43 @@ var products = []*Product{
 
 func GetProducts() []*Product {
 	return products
+}
+
+func FindProductByID(id int) (*Product, int, error) {
+	product := &Product{}
+	index := -1
+
+	for i, p := range products {
+		if p.ID == id {
+			product = p
+			index = i
+		}
+	}
+
+	if index == -1 {
+		return nil, index, errors.New("Product Not Found")
+	}
+
+	return product, index, nil
+}
+
+func Contains(p *Product) bool {
+	return funk.Contains(products, p)
+}
+
+func DeleteProductByID(id int) error {
+	product, i, err := FindProductByID(id)
+
+	if err != nil {
+		return err
+	}
+
+	last := products[len(products)-1]
+	products[i] = last
+	last = product
+
+	products = funk.Initial(products).([]*Product)
+	return nil
 }
 
 func AddProduct(p *Product) {
